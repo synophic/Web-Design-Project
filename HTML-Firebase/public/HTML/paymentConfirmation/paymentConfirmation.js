01c45aca902ea17543e68fd10a55ab0e12186432
ArayaThongleg <61070270@kmitl.ac.th>
2021-05-05 14:12:14 +0700

    oil-index-pay

--- HTML-Firebase/public/HTML/paymentConfirmation/paymentConfirmation.js
+++ HTML-Firebase/public/HTML/paymentConfirmation/paymentConfirmation.js
@@ -1,73 +1,69 @@
-var user = firebase.database().ref("user");
-var product = firebase.database().ref("product");
-var soldRec = firebase.database().ref("soldRec");
-var catagory = firebase.database().ref("catagory");
+// var user = firebase.database().ref("user");
+var Payment_slip = firebase.database().ref("img_slip");
+// var catagory = firebase.database().ref("catagory");
 
 let CATAGORY_LIST_CONTAINER = document.querySelector(".catg-list");
 
-let user_data;
-let prod_data;
-let sold_data;
-let catg_data;
+let day_data;
+let time_data;
 
 function getData() {
-  user.orderByChild("id").on("value", (snapshot) => {
+
+  img_slip.orderByChild("id").on("value", (snapshot) => {
     if (snapshot.exists()) {
-    	user_data = snapshot.val();
+      user_data = snapshot.val();
     } else {
       console.log("No User data available");
       user_data = "";
-      // 1.2) Set Display condition: When it doesn't have any data you must hide a remove button.
-    }
-  });
-  product.orderByChild("id").on("value", (snapshot) => {
-    if (snapshot.exists()) {
-    	prod_data = snapshot.val();
-    } else {
-      console.log("No Product data available");
-      prod_data_data = "";
-      // 1.2) Set Display condition: When it doesn't have any data you must hide a remove button.
-    }
-  });
-  soldRec.orderByChild("id").on("value", (snapshot) => {
-    if (snapshot.exists()) {
-    	sold_data = snapshot.val();
-    } else {
-      console.log("No Sold record data available");
-      sold_data = "";
-      // 1.2) Set Display condition: When it doesn't have any data you must hide a remove button.
-    }
-  });
-  catagory.orderByChild("id").on("value", (snapshot) => {
-    if (snapshot.exists()) {
-    	catg_data = snapshot.val();
-    	renderCatagoryList(catg_data);
-    } else {
-      console.log("No Catagory data available");
-      catg_data = "";
-      // 1.2) Set Display condition: When it doesn't have any data you must hide a remove button.
     }
   });
 
   // 1.3) Get "runId" value from Realtime database. then store data to "RUN_ID" variable.
 }
 
-function renderCatagoryList(catagoryList){
-	CATAGORY_LIST_CONTAINER.innerHTML = "";
-	for (let i in catagoryList) {
-		CATAGORY_LIST_CONTAINER.appendChild(
-			createCatagoryList(catagoryList[i])
-			);
-	}
-}
+function uploadData() {
+  let name = document.querySelector("#name").value;
+  let brand = document.querySelector("#brand").value;
+  
 
-function createCatagoryList(text){
-	const li = document.createElement("li");
-	const a = document.createElement("a");
-	li.classList.add("catg-item");
-	a.innerHTML = text;
-	li.appendChild(a);
-	return li;
+  if ((name != "") &&
+    (brand != "") &&
+    (quantity != 0) &&
+    (price != 0) &&
+    (desc != "") &&
+    document.querySelector("#files").files.length != 0) {
+    let newProdRef = productRef.push({
+      "name": name,
+      "brand": brand,
+      "quantity": parseFloat(quantity),
+      "price": parseInt(price),
+      "description": desc
+    });
+
+    for (var i = 0; i < document.querySelector("#files").files.length; i++) {
+      let file = document.querySelector("#files").files[i];
+      let date = new Date().getTime();
+
+      let thisRef = storageRef.child(newProdRef.key + "/" + date + "_" + file.name);
+
+      thisRef.put(file).then((snapshot) => {
+        thisRef.getDownloadURL().then((url) => {
+          newProdRef.child("images").push(url);
+        });
+      }).catch(e => {
+        console.log('Error' + e);
+      });
+    }
+    alert("Upload Success")
+
+  }
+  else {
+    if (name == "") document.querySelector("#nameAlert").style.display = "initial";
+    if (brand == "") document.querySelector("#nameAlert").style.display = "initial";
+    if (quantity == "") document.querySelector("#nameAlert").style.display = "initial";
+    if (price == "") document.querySelector("#nameAlert").style.display = "initial";
+    if (document.querySelector("#files").files.length == 0) document.querySelector("#nameAlert").style.display = "initial";
+  }
 }
 
 window.onload = getData();
